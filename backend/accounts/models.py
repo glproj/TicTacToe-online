04380bb99bb593accf2d0663407818a3f14a51a8
glproj <glprojetinho@gmail.com>
2021-10-19 20:27:20 -0300

    feat(accounts): make username unique

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,18 +3,22 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
 
+
 class CustomUser(AbstractUser):
     username_validator = UnicodeUsernameValidator()
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField(_("email address"), unique=True)
     username = models.CharField(
-    _('username'),
-    max_length=150,
-    help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-    validators=[username_validator],
-    error_messages={
-        'unique': _("A user with that username already exists."),
-    },
-    blank=True,
-)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS= ['username']
+        _("username"),
+        max_length=150,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+        unique=True,
+        blank=True,
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
