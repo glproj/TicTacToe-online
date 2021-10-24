26d7304c0f754c8319a2569527d2ec907669a5de
glproj <glprojetinho@gmail.com>
2021-10-23 21:02:49 -0300

    feat(accounts): remove views.py and serializers.py
    Djoser already handles these things.

--- backend/accounts/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import get_user_model
-from accounts.serializers import CustomUserSerializer
-from .models import CustomUser
-from rest_framework import viewsets
-User = get_user_model()
-
-class CustomUserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
