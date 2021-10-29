bab733951c879251860f865b1ec21cc90a834c6d
glproj <glprojetinho@gmail.com>
2021-10-29 14:59:08 -0300

    feat(history): add moves field to Game model

--- backend/history/models.py
+++ backend/history/models.py
@@ -11,3 +11,10 @@ class Game(models.Model):
     )
     started = models.DateTimeField()
     finished = models.DateTimeField(auto_now_add=True)
+    moves = models.CharField(max_length=10) # the first character is the first mark played.
+    # The rest of the characters are the coordinates of the moves
+    # example: 'x3571986' correspond to the moves at 
+    # https://codepen.io/denypatrascu/pen/pvYdYg          |     |
+    # 1 2 3
+    # 4 5 6
+    # 7 8 9
--- backend/history/serializers.py
+++ backend/history/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Game
+
+
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = ("winner", "loser", "started", "finished")
+        fields = ("winner", "loser", "started", "finished", "moves")
