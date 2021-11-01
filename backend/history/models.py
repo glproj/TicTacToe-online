d56cbc17d706540a08b35a50fce771df5a245b76
glproj <glprojetinho@gmail.com>
2021-11-01 16:58:26 -0300

    feat(backend): add played_by field in Game model
    Now we can know who played a game in case of a draw.

--- backend/history/models.py
+++ backend/history/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Game(models.Model):
+    played_by = models.ManyToManyField(User)
     winner = models.ForeignKey(
         get_user_model(), on_delete=models.SET_NULL, null=True, related_name="winner"
     )
