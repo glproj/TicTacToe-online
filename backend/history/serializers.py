f1aed53dac30c7ef66eb986bd7a51bfcdc993b7d
glproj <glprojetinho@gmail.com>
2021-11-01 17:29:53 -0300

    feat(backend): add played_by field to GameSerializer

--- backend/history/serializers.py
+++ backend/history/serializers.py
@@ -5,6 +5,7 @@ from .models import Game
 class GameSerializer(serializers.ModelSerializer):
     winner = serializers.SlugRelatedField(slug_field="username", read_only=True)
     loser = serializers.SlugRelatedField(slug_field="username", read_only=True)
+    played_by = serializers.SlugRelatedField(slug_field="username", many=True, read_only=True)
     class Meta:
         model = Game
-        fields = ("winner", "loser", "started", "finished", "moves")
+        fields = ("played_by", "winner", "loser", "started", "finished", "moves")
