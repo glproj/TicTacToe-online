fc231aee9c55497f36e5002ec120002effe43e74
glproj <glprojetinho@gmail.com>
2021-11-01 15:16:46 -0300

    feat(backend): use username instead of id in GameSerializer
    Winner and loser fields now show the username of the user instead of the id.

--- backend/history/serializers.py
+++ backend/history/serializers.py
@@ -3,6 +3,8 @@ from .models import Game
 
 
 class GameSerializer(serializers.ModelSerializer):
+    winner = serializers.SlugRelatedField(slug_field="username", read_only=True)
+    loser = serializers.SlugRelatedField(slug_field="username", read_only=True)
     class Meta:
         model = Game
         fields = ("winner", "loser", "started", "finished", "moves")
