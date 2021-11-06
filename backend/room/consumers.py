88a24678357a7f2810d0b6de6050b129a9555377
glproj <glprojetinho@gmail.com>
2021-11-06 11:51:23 -0300

    feat(backend): add a basic consumer that sends the positions played to the players

--- backend/room/consumers.py
+++ backend/room/consumers.py
@@ -1,6 +1,55 @@
 # chat/consumers.py
 import json
 from channels.generic.websocket import WebsocketConsumer
+from .models import Room
+from asgiref.sync import async_to_sync
+
 
 class RoomConsumer(WebsocketConsumer):
-    pass
+    def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = "game_" + self.room_name
+        try:
+            room = Room.objects.get(name=self.room_name)
+            player_number = room.nb_of_players
+            if player_number == 2:
+                self.close()
+            else:
+                room.nb_of_players += 1
+                room.save()
+                async_to_sync(self.channel_layer.group_add)(
+                    self.room_group_name, self.channel_name
+                )
+
+                self.accept()
+        except Room.DoesNotExist:
+            room = Room.objects.create(name=self.room_name, nb_of_players=1)
+            async_to_sync(self.channel_layer.group_add)(
+                self.room_group_name, self.channel_name
+            )
+
+            self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
+        room = Room.objects.get(name=self.room_name)
+        player_number = room.nb_of_players
+        # if player_number == 2:
+        #     return
+        room.delete()
+
+    def receive(self, text_data):
+        data_json = json.loads(text_data)
+        position = data_json["position"]
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "position_played", "position": position}
+        )
+
+    def position_played(self, event):
+        try:
+            self.played += event["position"]
+        except AttributeError:
+            self.played = event["position"]
+        self.send(json.dumps({"position": event["position"]}))
