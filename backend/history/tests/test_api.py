a39ddc1638f3be778de8038039db34a9cdd75962
glproj <glprojetinho@gmail.com>
2021-11-06 18:07:35 -0300

    test(backend): add one more game and user to the test database of GameApiTestCase

--- backend/history/tests/test_api.py
+++ backend/history/tests/test_api.py
@@ -18,16 +18,21 @@ class GameApiTestCase(APITestCase):
         user2 = User.objects.create_user(
             email="example2@email.com", username="example2", password="example123"
         )
-
+        user3 = User.objects.create_user(
+            email="example3@email.com", username="example3", password="example123"
+        )
         self.setup_datetime = datetime(
             year=2021, month=11, day=21, hour=13, minute=12, tzinfo=pytz.UTC
         )
 
         game = Game(started=self.setup_datetime - timedelta(minutes=2))
+        game1 = Game(started=self.setup_datetime - timedelta(minutes=10))
         game_tie = Game(
             started=self.setup_datetime - timedelta(minutes=5),
         )
+
         game.save()
+        game1.save()
         game_tie.save()
 
         game.played_by.set([user1, user2])
@@ -36,50 +41,70 @@ class GameApiTestCase(APITestCase):
         game.moves = "x3571986"
         game.finished = self.setup_datetime
 
+        game1.played_by.set([user2, user3])
+        game1.winner = user3
+        game1.loser = user2
+        game1.moves = "x3571986"
+        game1.finished = self.setup_datetime - timedelta(minutes=7)
+
         game_tie.played_by.set([user1, user2])
         # since the game is a tie, there are no winners.
         game_tie.moves = "o215368794"
         game_tie.finished = self.setup_datetime - timedelta(minutes=3)
 
         game.save()
+        game1.save()
         game_tie.save()
 
     def test_listing_games(self):
         game_list_url = reverse("game-list")
         response = self.client.get(game_list_url)
-        self.assertEqual(
-            response.data,
-            [
-                collections.OrderedDict(
-                    {
-                        "played_by": ["example1", "example2"],
-                        "winner": "example1",
-                        "loser": "example2",
-                        "started": (
-                            self.setup_datetime - timedelta(minutes=2)
-                        ).strftime(self.serializer_datetime_format),
-                        "finished": self.setup_datetime.strftime(
-                            self.serializer_datetime_format
-                        ),
-                        "moves": "x3571986",
-                    }
-                ),
-                collections.OrderedDict(
-                    {
-                        "played_by": ["example1", "example2"],
-                        "winner": None,
-                        "loser": None,
-                        "started": (
-                            self.setup_datetime - timedelta(minutes=5)
-                        ).strftime(self.serializer_datetime_format),
-                        "finished": (
-                            self.setup_datetime - timedelta(minutes=3)
-                        ).strftime(self.serializer_datetime_format),
-                        "moves": "o215368794",
-                    }
-                ),
-            ],
-        )
+        expected_value = [
+            collections.OrderedDict(
+                {
+                    "played_by": ["example1", "example2"],
+                    "winner": "example1",
+                    "loser": "example2",
+                    "started": (self.setup_datetime - timedelta(minutes=2)).strftime(
+                        self.serializer_datetime_format
+                    ),
+                    "finished": self.setup_datetime.strftime(
+                        self.serializer_datetime_format
+                    ),
+                    "moves": "x3571986",
+                }
+            ),
+            collections.OrderedDict(
+                {
+                    "played_by": ["example2", "example3"],
+                    "winner": "example3",
+                    "loser": "example2",
+                    "started": (self.setup_datetime - timedelta(minutes=10)).strftime(
+                        self.serializer_datetime_format
+                    ),
+                    "finished": (self.setup_datetime - timedelta(minutes=7)).strftime(
+                        self.serializer_datetime_format
+                    ),
+                    "moves": "x3571986",
+                }
+            ),
+            collections.OrderedDict(
+                {
+                    "played_by": ["example1", "example2"],
+                    "winner": None,
+                    "loser": None,
+                    "started": (self.setup_datetime - timedelta(minutes=5)).strftime(
+                        self.serializer_datetime_format
+                    ),
+                    "finished": (self.setup_datetime - timedelta(minutes=3)).strftime(
+                        self.serializer_datetime_format
+                    ),
+                    "moves": "o215368794",
+                }
+            ),
+        ]
+        for i in expected_value:
+            self.assertEqual(i, response.data[expected_value.index(i)])
 
     def test_detail_of_normal_game(self):
         game_detail_url = reverse("game-detail", args=[1])
@@ -101,7 +126,7 @@ class GameApiTestCase(APITestCase):
         )
 
     def test_detail_of_tie_game(self):
-        game_detail_url = reverse("game-detail", args=[2])
+        game_detail_url = reverse("game-detail", args=[3])
         response = self.client.get(game_detail_url)
         self.assertEqual(
             response.data,
