f4ddec78801ab45ddbfaf2430a8029c512face68
glproj <glprojetinho@gmail.com>
2021-11-06 11:48:27 -0300

    feat(backend): add support for debugging inside docker

--- backend/manage.py
+++ backend/manage.py
@@ -3,6 +3,13 @@
 import os
 import sys
 
+def initialize_debugger():
+    import debugpy
+    if not os.getenv("RUN_MAIN") and os.getenv("ENV") == "DEBUG":
+        debugpy.listen(("0.0.0.0", 5678))
+        sys.stdout.write("Start the VS Code debugger now, waiting...\n")
+        debugpy.wait_for_client()
+        sys.stdout.write("Debugger attached, starting server...\n")
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +22,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    initialize_debugger()
     execute_from_command_line(sys.argv)
 
 
