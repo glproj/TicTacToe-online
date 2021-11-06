ac8eed14b20e4bacd80ea34626b7c6dc1262a815
glproj <glprojetinho@gmail.com>
2021-11-06 20:56:26 -0300

    feat(backend): use postgres instead of sqlite

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -90,11 +90,14 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
