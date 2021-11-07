a1acdaadc582c27dcba7be7ea818146a284ec741
glproj <glprojetinho@gmail.com>
2021-11-07 13:01:06 -0300

    feat(backend): add __str__ method to Room model

--- backend/room/models.py
+++ backend/room/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=50)
     nb_of_players = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}, {self.nb_of_players} player(s)"
