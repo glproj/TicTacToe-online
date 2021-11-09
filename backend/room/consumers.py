759d962cd6b56bd9104fd77f1b1b969a4cc9deee
glproj <glprojetinho@gmail.com>
2021-11-09 10:40:56 -0300

    feat(room): add a result system to the game

--- backend/room/consumers.py
+++ backend/room/consumers.py
@@ -1,11 +1,22 @@
 # chat/consumers.py
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 from .models import Room
 from asgiref.sync import async_to_sync
 
+winning_positions = [
+    "123",
+    "456",
+    "789",
+    "147",
+    "258",
+    "369",
+    "159",
+    "357",
+]
 
-class RoomConsumer(WebsocketConsumer):
+
+class RoomConsumer(JsonWebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "game_" + self.room_name
@@ -54,4 +65,28 @@ class RoomConsumer(WebsocketConsumer):
             self.played += event["position"]
         except AttributeError:
             self.played = event["position"]
-        self.send(json.dumps({"position": event["position"]}))
+
+        current_player = 1 if len(self.played) % 2 == 1 else 2
+        played_by_current_player = ""
+        # this block will pick only the moves
+        # of the player that made the last move
+        for i in range(len(self.played)):
+            if i % 2 != len(self.played) % 2:
+                played_by_current_player += self.played[i]
+
+        for win in winning_positions:
+            if set(win).issubset(set(played_by_current_player)):
+                self.send_json(
+                    {
+                        "position": event["position"],
+                        "result": f"player{current_player}_win",
+                    }
+                )
+            elif len(self.played) == 9:
+                self.send_json(
+                    {
+                        "position": event["position"],
+                        "result": "draw",
+                    }
+                )
+        self.send_json({"position": event["position"]})
