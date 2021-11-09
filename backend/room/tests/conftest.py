d6156e8dad035717bbb3c045848ea6f334adf0b2
glproj <glprojetinho@gmail.com>
2021-11-09 17:07:36 -0300

    fix(room): fix test database setup

--- backend/room/tests/conftest.py
+++ backend/room/tests/conftest.py
@@ -2,38 +2,46 @@ import pytest
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from channels.testing import WebsocketCommunicator
-from history.models import Game
 from config.asgi import application
 
 User = get_user_model()
 
-@pytest.fixture
-def create_users_and_tokens(django_db_setup):
-    example1 = User.objects.create_user(
-        email="example1@email.com",
-        username="example1",
-        password="example123",
-        is_active=True,
-    )
-    example2 = User.objects.create_user(
-        email="example2@email.com",
-        username="example2",
-        password="example123",
-        is_active=True,
-    )
-    client = APIClient()
-    example1_tokens = client.post(
-        "/api/v1/auth/jwt/create/",
-        {"email": "example1@email.com", "password": "example123"},
-    ).data
-    example2_tokens = client.post(
-        "/api/v1/auth/jwt/create/",
-        {"email": "example2@email.com", "password": "example123"},
-    ).data
-    return example1_tokens["access"], example2_tokens["access"]
+
+@pytest.fixture(scope="session")
+def create_users_and_tokens(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        anonymous = User.objects.create_user(
+            email="anonymous@anonymous.com",
+            username="anonymous",
+            password="v157894375n87189437r89ieuwionrc¨#&*¨*@!(*&#",
+            is_active=True,
+        )
+        example1 = User.objects.create_user(
+            email="example1@email.com",
+            username="example1",
+            password="example123",
+            is_active=True,
+        )
+        example2 = User.objects.create_user(
+            email="example2@email.com",
+            username="example2",
+            password="example123",
+            is_active=True,
+        )
+        client = APIClient()
+        example1_tokens = client.post(
+            "/api/v1/auth/jwt/create/",
+            {"email": "example1@email.com", "password": "example123"},
+        ).data
+        example2_tokens = client.post(
+            "/api/v1/auth/jwt/create/",
+            {"email": "example2@email.com", "password": "example123"},
+        ).data
+        return example1_tokens["access"], example2_tokens["access"]
+
 
 @pytest.fixture
-async def connect_players(create_users_and_tokens):
+async def connect_players(create_users_and_tokens, db):
     """
     Connects player1 and player2 to the lobby room.
     After the test is run, it disconnects the players.
