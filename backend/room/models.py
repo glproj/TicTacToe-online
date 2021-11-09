748bbe626e3b37190b7523fef1a04fdae19f964c
glproj <glprojetinho@gmail.com>
2021-11-09 09:40:34 -0300

    feat(backend): add players field to Room model

--- backend/room/models.py
+++ backend/room/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 class Room(models.Model):
     name = models.CharField(max_length=50)
+    players = models.ManyToManyField(get_user_model())
     nb_of_players = models.IntegerField()
 
     def __str__(self):
