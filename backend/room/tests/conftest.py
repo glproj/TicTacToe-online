2d3dc82138d8bed08047636486a6b1f0f791a69e
glproj <glprojetinho@gmail.com>
2021-11-09 09:49:47 -0300

    feat(room): add fixture that creates users and return their jwt access tokens

--- backend/room/tests/conftest.py
+++ backend/room/tests/conftest.py
@@ -5,6 +5,33 @@ from channels.testing import WebsocketCommunicator
 from history.models import Game
 from config.asgi import application
 
+User = get_user_model()
+
+@pytest.fixture
+def create_users_and_tokens(django_db_setup):
+    example1 = User.objects.create_user(
+        email="example1@email.com",
+        username="example1",
+        password="example123",
+        is_active=True,
+    )
+    example2 = User.objects.create_user(
+        email="example2@email.com",
+        username="example2",
+        password="example123",
+        is_active=True,
+    )
+    client = APIClient()
+    example1_tokens = client.post(
+        "/api/v1/auth/jwt/create/",
+        {"email": "example1@email.com", "password": "example123"},
+    ).data
+    example2_tokens = client.post(
+        "/api/v1/auth/jwt/create/",
+        {"email": "example2@email.com", "password": "example123"},
+    ).data
+    return example1_tokens["access"], example2_tokens["access"]
+
 @pytest.fixture
 async def connect_players(create_users_and_tokens):
     """
